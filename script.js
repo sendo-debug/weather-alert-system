3a2d89acc0f55c7eb2347ed647c00747505b130e
sendo-debug <sanjaykumar060579@gmail.com>
2026-09-06 17:31:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 async function getWeather() {
-    const response = await fetch(
-        "http://localhost:8080/weather?lat=28.3670&lon=79.4304"
-    );
+    try {
+        const response = await fetch(
+            "http://localhost:8080/weather?lat=28.3670&lon=79.4304"
+        );
 
-    const data = await response.json();
+        if (!response.ok) {
+            throw new Error("Backend request failed");
+        }
 
-    console.log(data);
+        const data = await response.json();
+
+        console.log("Weather data:", data);
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
 
 getWeather();
