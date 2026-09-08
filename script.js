864276fd226b4d51cbe364a75af1aa65666e309a
sendo-debug <sanjaykumar060579@gmail.com>
2026-09-08 10:43:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -286,20 +286,19 @@ if (window.__weatherguardScriptLoaded) {
 
             style.textContent = `
 
-                #mapFrame.weatherguard-risk-map {
-                    width: 100%;
-                    height: 500px;
-                    min-height: 500px;
-                    background: #111;
-                    position: relative;
-                    z-index: 1;
-                }
+            class MapManager {
+  #mapFrame; // Declaration is required
 
-                #mapFrame.weatherguard-risk-map .leaflet-container {
-                    width: 100%;
-                    height: 100%;
-                    font-family: Inter, sans-serif;
-                }
+  constructor(elementId) {
+    this.#mapFrame = document.getElementById(elementId);
+  }
+
+  getFrame() {
+    return this.#mapFrame;
+  }
+}
+
+const myMap = new MapManager('map');
 
                 .weatherguard-map-legend {
                     background: rgba(15, 15, 15, 0.92);
